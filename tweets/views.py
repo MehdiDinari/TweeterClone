e1cc21b2b1925d9176aed90e7512e0a8b018cb90
Mehdi Dinari <mehdi.dinari@usmba.ac.ma>
2025-03-08 23:46:05 +0000

    first commit

---
+++ tweets/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import tweet
+# Create your views here.
+def home_view(request ,*args, **kwargs):
+    return render(request,"pages/home.html", context={} ,status=200)
+
+def tweet_detail_view(request,tweet_id ,*args, **kwargs):
+    data = {
+    "id": tweet_id,
+       
+    } 
+      
+    try:
+     obj =tweet.objects.get(id=tweet_id)
+     data['content']=obj.content
+    except:
+     data['message']="Not found"
+     status =404
+
+  
+    return JsonResponse(data,status=status)
